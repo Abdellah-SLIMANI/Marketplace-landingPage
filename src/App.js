e5f16779b73bc429c32b564c2a6742aad2f770d3
morscor21@gmail.com <morscor21@gmail.com>
2021-05-10 22:29:41 +0000

    postponing scroll till we implement some functionalities / discord webhook test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.scss';
 import Header from './Components/Header/Header';
 import Drawing from './Components/Drawing/Drawing';
 import Description from './Components/Description/Description';
+import useSticky from "./Components/Header/scrollHandler"
 
 function App() {
+  // const { isSticky, element } = useSticky()
   return (
     <div className="App">
       <Header />
